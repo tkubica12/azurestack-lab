6430e147a01cd661ac818fb158da0047ccd7cd1e
Tomas Kubica <tkubica12@gmail.com>
2020-06-24 20:37:10 +0200

    ARM template for few policies for Azure Arc for Kubernetes

--- demo-environment/arm/azure-deploy.ps1
+++ demo-environment/arm/azure-deploy.ps1
@@ -4,3 +4,6 @@ az account set -s AzureStackCZSK
 # Deploy monitoring
 az group create -n monitoring-rg -l westeurope
 az group deployment create -g monitoring-rg --template-file azure-monitoring.json
+
+# Deploy Azure Policy for Kubernetes
+az deployment sub create -l westeurope --template-file azure-kubernetes-policy.json
